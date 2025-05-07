e54df67d47d5384efba84d0b262dc927d41b9692
tyty1979 <aguilerabraian@gmail.com>
2025-05-07 17:40:32 -0300

    Add files via upload

---
+++ juego-pocoyo-pwa/sw.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'pocoyo-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './running-43089.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
